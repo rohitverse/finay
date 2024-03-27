08e8b50503f2e6f783f27645a1093b2cd13c2f9b
rohitverse <rohitkr23072001@gmail.com>
2024-03-27 13:02:38 +0530

    props passed to songCard

--- src/Pages/Profile/Library/Songs/SongCard/SongCard.js
+++ src/Pages/Profile/Library/Songs/SongCard/SongCard.js
@@ -5,15 +5,15 @@ import songPost from "../../../../../Assets/Images/songImage.svg";
 import CustomButton from "../../../../../Components/Common/CustomButton/CustomButton";
 import { Diamond } from "../../../../../Assets/Images/Svgicon";
 
-const SongCard = () => {
+const SongCard = ({ username, headCaption, editions, value }) => {
   return (
     <>
-      <div className="songCard my-3 p-2">
+      <div className="songCard my-3 p-2 me-3">
         <div className="songCardHead d-flex px-2 pt-2">
           <figure className=" pt-1">
             <img src={UserImg} alt="User" className="imagePost" />
           </figure>
-          <div className="p-2 me-2 small">@Enimen</div>
+          <div className="p-2 me-2 small">{username}</div>
           <div className="songCard_Content p-2 ">
             <div className="hashtag small">#Frameworks</div>
           </div>
@@ -22,20 +22,24 @@ const SongCard = () => {
           <figure className="p-2">
             <img src={songPost} alt="song " />
           </figure>
-          <h4 className="mx-2"> Venom Forest Prince</h4>
+          <h4 className="mx-2"> {headCaption}</h4>
           <div className="d-flex justify-content-between mx-2">
-            <div>no. of editions 50</div>
+            <div>no. of editions: {editions}</div>
             <div className="diamond me-1">
               <span className="me-2">
                 <Diamond />
               </span>
-              2.75
+              {value}
             </div>
           </div>
           <div className="hr m-2"></div>
           <div className="d-flex">
             <CustomButton className="" type="button" text="Buy" />
-            <CustomButton className="makeOffer" type="button" text="Make Offer" />
+            <CustomButton
+              className="makeOffer"
+              type="button"
+              text="Make Offer"
+            />
           </div>
         </div>
       </div>
--- src/Pages/Profile/Library/Songs/Songs.js
+++ src/Pages/Profile/Library/Songs/Songs.js
@@ -2,10 +2,64 @@ import React from "react";
 import SongCard from "./SongCard/SongCard";
 
 const Songs = () => {
+  const songData = [
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+    {
+      username: "@Enimen",
+      headCaption: "Lorem placeat ",
+      editions: "10",
+      value: 10,
+    },
+  ];
   return (
     <>
-      <div className="songs"><SongCard/>
-        
+      <div className="songs my-3 d-flex  flex-wrap ">
+        {songData.map((item, index) => {
+          return (
+            <SongCard
+              key={index}
+              username={item.username}
+              editions={item.editions}
+              headCaption={item.headCaption}
+              value={item.value}
+            />
+          );
+        })}
       </div>
     </>
   );
